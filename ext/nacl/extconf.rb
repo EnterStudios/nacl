4949a5a383609e1d22ccde68bae08c3793c1b2a7
rep <ms@mwcollect.org>
2012-09-19 18:29:33 +0200

    hardcoded paths, to be replaced by env var

--- ext/nacl/extconf.rb
+++ ext/nacl/extconf.rb
@@ -1,4 +1,8 @@
 require 'mkmf'
 
+$LDFLAGS << '-Wl,-Bsymbolic-functions -fPIC -lnacl /opt/pynacl/nacl-20110221/build/rep220/lib/amd64/randombytes.o -L/opt/pynacl/nacl-20110221/build/rep220/lib/amd64'
+
+$CFLAGS << ' -fPIC -I/opt/pynacl/nacl-20110221/build/rep220/include/amd64 '
+
 have_library "nacl"
 create_makefile('nacl/nacl')
